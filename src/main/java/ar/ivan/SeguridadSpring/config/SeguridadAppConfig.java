88abf2c639513d13de496d321e8784b7fcb70bd7
Iván Aguilar <ivan_2003sr@hotmail.com>
2022-07-26 16:47:18 -0300

    Login Spring por defecto

---
+++ src/main/java/ar/ivan/SeguridadSpring/config/SeguridadAppConfig.java
@@ -0,0 +1,33 @@
+package ar.ivan.SeguridadSpring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SeguridadAppConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+
+		UserBuilder usuarios=User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(usuarios.username("Ivan").password("123").roles("administrador"))
+		.withUser(usuarios.username("Evelyn").password("123456").roles("usuario"))
+		.withUser(usuarios.username("Valentín").password("456").roles("ayudante"))
+		.withUser(usuarios.username("Ailen").password("2468").roles("administrador"));
+		
+		
+	}
+	
+	
+
+	
+	
+}
